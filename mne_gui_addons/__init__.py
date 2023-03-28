f76a7edb8cff224c5850837d568d237de7ebca4d
Eric Larson <larson.eric.d@gmail.com>
2023-03-28 08:48:58 -0600

    MAINT: Fix actions (#2)

--- mne_gui_addons/__init__.py
+++ mne_gui_addons/__init__.py
@@ -4,4 +4,4 @@ try:
     __version__ = version("mne_gui_addons")
 except PackageNotFoundError:
     # package is not installed
-    __version__ = "0.0.0"
+    __version__ = "0.0.0"  # pragma: no cover
