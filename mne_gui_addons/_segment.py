55a0b30998423a7336c90ccbc4a1786ab5d53819
Alex Rockhill <aprockhill@mailbox.org>
2023-04-25 10:03:58 -0700

    black

--- mne_gui_addons/_segment.py
+++ mne_gui_addons/_segment.py
@@ -252,16 +252,21 @@ class VolumeSegmenter(SliceBrowser):
             "Applying the brainmask, this will take ~30 seconds",
         )
         img_data, _, vox_scan_ras_t, _ = _load_image(
-            op.join(self._subject_dir, "mri", "brainmask.mgz"))
-        idxs = np.meshgrid(np.arange(self._base_data.shape[0]),
-                           np.arange(self._base_data.shape[1]),
-                           np.arange(self._base_data.shape[2]),
-                           indexing='ij')
+            op.join(self._subject_dir, "mri", "brainmask.mgz")
+        )
+        idxs = np.meshgrid(
+            np.arange(self._base_data.shape[0]),
+            np.arange(self._base_data.shape[1]),
+            np.arange(self._base_data.shape[2]),
+            indexing="ij",
+        )
         idxs = np.array(idxs)  # (3, *image_data.shape)
         idxs = np.transpose(idxs, [1, 2, 3, 0])  # (*image_data.shape, 3)
         idxs = idxs.reshape(-1, 3)  # (n_voxels, 3)
         idxs = apply_trans(self._vox_scan_ras_t, idxs)  # vox -> scanner RAS
-        idxs = apply_trans(np.linalg.inv(vox_scan_ras_t), idxs)  # scanner RAS -> mri vox
+        idxs = apply_trans(
+            np.linalg.inv(vox_scan_ras_t), idxs
+        )  # scanner RAS -> mri vox
         idxs = idxs.round().astype(int)  # round to nearest voxel
         brain = set([(x, y, z) for x, y, z in np.array(np.where(img_data > 0)).T])
         mask = np.array([tuple(idx) not in brain for idx in idxs])
