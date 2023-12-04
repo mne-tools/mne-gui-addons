aef86070e1bbefb5928af9b372ea6fc49bb39e27
Alex Rockhill <aprockhill@mailbox.org>
2023-12-04 15:33:00 -0800

    cruft

--- mne_gui_addons/conftest.py
+++ mne_gui_addons/conftest.py
@@ -1,9 +1,6 @@
 # get all MNE fixtures and settings
 from mne.conftest import *  # noqa: F403
 
-import warnings
-
-
 def pytest_configure(config):
     """Configure pytest options."""
     config.addinivalue_line(
